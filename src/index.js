c6b7671bb05c13536e0b4179a152cbffc273b41c
Aviad Hahami <aviadhahami@gmail.com>
2017-05-25 20:43:01 +0300

    wooo

--- src/index.js
+++ src/index.js
@@ -15,9 +15,6 @@ app.use(bodyParser.json({
 	limit: '100kb'
 }));
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-
 
 const router = express.Router();
 router.get('*', (req, res) =>{
@@ -31,44 +28,6 @@ router.get('*', (req, res) =>{
 			return;
 		}
 
-		websites
-			.findOne({ 'url': subdomainName, 'active': true })
-			.select('accountId')
-			.exec((err, website) =>{
-				if(err || !website){
-					const statusCode = err ? 500 : 404;
-					res.status(statusCode).render('error',
-						{ subdomainName, statusCode, error: err || 'Not found' });
-					return;
-				}
-
-				const { _id, accountId } = website;
-				fetchClientToken(_id, accountId).then(tokenResponse =>{
-
-					const appConfig = tokenResponse;
-
-
-					let viewToRender, productConfig;
-					switch(appConfig.type){
-						case (PRODUCT_TYPES.WEBSITE):
-						default:{
-							viewToRender = 'website';
-							productConfig = {
-								layout: false,
-								config: JSON.stringify(appConfig || {})
-							};
-						}
-					}
-
-					res.render(viewToRender, productConfig);
-					return;
-				}, fetcherErr =>{
-					const { statusCode, error } = fetcherErr;
-					res.status(statusCode).render('error', { subdomainName, statusCode, error });
-					return;
-				});
-			})
-
 	}catch(runtimeErr){
 		res.status(500).json(runtimeErr);
 		return;
@@ -76,7 +35,7 @@ router.get('*', (req, res) =>{
 });
 
 // Listen to everything
-app.use(subdomain('*', router));
+app.use('*', router);
 
 
 app.server.listen(process.env.PORT || '3002');
