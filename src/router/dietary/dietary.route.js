f23dfb5814f30fc5837f1af8a9847a613bbfb586
Aviad Hahami <aviadhahami@gmail.com>
2017-05-25 21:26:56 +0300

    tsting

--- src/router/dietary/dietary.route.js
+++ src/router/dietary/dietary.route.js
@@ -12,7 +12,15 @@ const dietOptionsAsObject = {};
 dietOptions.forEach((option, index) => dietOptionsAsObject[index] = option);
 
 const controller = (req, res) => {
-	res.status(200).json({ 'text': dietOptionsAsObject });
+	// res.status(200).json({ 'text': dietOptionsAsObject });
+	const response = {
+		"messages": [
+			{ "text": "Welcome to our store!" },
+			{ "text": "How can I help you?" }
+		]
+	}
+	res.status(200).json(response);
+	
 };
 
 const route = {
