a3d749ee6d2094828492a428b15a237926c18226
Aviad Hahami <aviadhahami@gmail.com>
2017-05-25 21:01:56 +0300

    version route ready

--- src/router/router.js
+++ src/router/router.js
@@ -1,5 +1,16 @@
 import express from 'express';
+import versionRoute from './version/version.route';
 
 
+const routes = [
+	versionRoute,
+];
 const router = express.Router();
+
+routes.forEach(route => {
+	console.log(route);
+	router[route.method.toLowerCase()](route.path, route.controller);
+});
+
+
 export default router;
--- src/router/version/version.route.js
+++ src/router/version/version.route.js
@@ -1,5 +1,14 @@
-const route = (req, res) => {
+import { version } from './../../../package.json';
 
+
+const routeController = (req, res) => {
+	res.status(200).send(version);
+};
+
+const route = {
+	path: '/version',
+	method: 'GET',
+	controller: routeController
 };
 
 export default route;
